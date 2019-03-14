158b8cf036be1617fcd1778f77d62e52bf5f27df
HanBitPix <hanjt517@gmail.com>
2019-03-14 14:09:48 -0700

    Updated the Budget: UI Controller

--- app.js
+++ app.js
@@ -108,7 +108,11 @@ const UIController = (() => {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -167,6 +171,19 @@ const UIController = (() => {
       });
     },
 
+    displayBudget: (obj) => {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if (obj.percentage > 0){
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      }
+      else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
+
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -199,7 +216,7 @@ const controller = ((budgetCtrl, UICtrl) => {
     const budget = budgetCtrl.getBudget();
 
     // Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   const ctrlAddItem = () => {
@@ -228,6 +245,12 @@ const controller = ((budgetCtrl, UICtrl) => {
   return {
     init: () => {
       console.log('Application has started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
